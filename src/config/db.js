5da1ee20219dfd1dbf761a3abf866db375dd537e
Luis Aneuris Tavarez De Jesus <luisaneuris60@gmail.com>
2020-03-12 20:04:57 -0400

    Changes database api

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,12 @@
 import Firebase from 'firebase'
 let config = {
-    apiKey: "AIzaSyAeqMem5dquIVXJBgZgdoUQDD-b-94m0TM",
-    authDomain: "sinuous-myth-244502.firebaseapp.com",
-    databaseURL: "https://sinuous-myth-244502.firebaseio.com",
-    projectId: "sinuous-myth-244502",
-    storageBucket: "sinuous-myth-244502.appspot.com",
-    messagingSenderId: "411726069095",
-    appId: "1:411726069095:web:8f96dbbdb7ba01e26f03c2",
-    measurementId: "G-B1GZS3L60B"
+    apiKey: "AIzaSyB0ZTEukIsLdmc2oba1H9DfEixSfLEecnw",
+    authDomain: "webprojects-55c97.firebaseapp.com",
+    databaseURL: "https://webprojects-55c97.firebaseio.com",
+    projectId: "webprojects-55c97",
+    storageBucket: "webprojects-55c97.appspot.com",
+    messagingSenderId: "383046242155",
+    appId: "1:383046242155:web:bb290d1037f19a0073062e"
 };
 let app = Firebase.initializeApp(config)
 export const db = app.database()
